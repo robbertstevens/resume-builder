d85bf90e0de905aa1b4a6ff94ef3df381a4fc17a
Robbert Stevens <robbertstevens@gmail.com>
2019-01-04 21:25:52 +0100

    remove of experiences

--- app.js
+++ app.js
@@ -1,51 +1,47 @@
 'use strict';
 
 const handlebars = require('handlebars');
-const Store = require('electron-store');
-
-let sections = [{
-    "title": "Education",
-    "experiences": [
-        {
-            "period": "sept. 2017 - present",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, amet cupiditate deserunt distinctio illo maxime nulla perferendis quisquam repellat repellendus? Dolorem, laborum, nulla. Dicta eveniet laborum nostrum, placeat qui vel."
-        },
-        {
-            "period": "aug. 800 - sept. 2017",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, amet cupiditate deserunt distinctio illo maxime nulla perferendis quisquam repellat repellendus? Dolorem, laborum, nulla. Dicta eveniet laborum nostrum, placeat qui vel."
-        }
-    ]
-},{
-    "title": "Experience",
-    "experiences": [
-        {
-            "period": "sept. 2017 - present",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, amet cupiditate deserunt distinctio illo maxime nulla perferendis quisquam repellat repellendus? Dolorem, laborum, nulla. Dicta eveniet laborum nostrum, placeat qui vel."
-        },
-        {
-            "period": "aug. 800 - sept. 2017",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, amet cupiditate deserunt distinctio illo maxime nulla perferendis quisquam repellat repellendus? Dolorem, laborum, nulla. Dicta eveniet laborum nostrum, placeat qui vel."
-        }
-    ]
-}];
+const { ipcRenderer } = require('electron');
 
 let source = document.getElementById('hb-experience-template').innerHTML;
 let template = handlebars.compile(source);
-let result = "";
 
-sections.forEach(function(context){
-    result = result + template(context);
+ipcRenderer.send('show');
+
+ipcRenderer.on("update", (event, resume) => {
+
+    let content = document.getElementById('cv');
+    content.innerHTML = "";
+    content.innerHTML = template(resume);
+
+    document.querySelectorAll(".description").forEach(function (element) {
+        element.addEventListener("blur", function (e) {
+            ipcRenderer.send('experience-edit', {
+                'content': this.innerHTML,
+                'section': this.dataset.section,
+                'experience': this.dataset.experience
+            });
+        });
+    });
+
+    document.querySelectorAll(".remove").forEach(function (element) {
+        element.addEventListener("click", function (e) {
+            ipcRenderer.send('experience-remove', {
+                'section': this.dataset.section,
+                'experience': this.dataset.experience
+            });
+        });
+    });
+
+    tinymce.init({
+        selector: '.description',
+        theme: "inlite",
+        inline: true,
+        plugins: 'lists',
+        selection_toolbar: 'bold italic underline numlist bullist',
+        insert_toolbar : false,
+    });
 });
 
-let content = document.getElementById('cv');
 
-content.innerHTML = content.innerHTML + result;
 
-tinymce.init({
-    selector: '.description',
-    theme: "inlite",
-    inline: true,
-    plugins: 'lists',
-    selection_toolbar: 'bold italic underline numlist bullist',
-    insert_toolbar : false
-});
--- main.js
+++ main.js
@@ -1,23 +1,82 @@
 'use strict';
-const { app, BrowserWindow } = require('electron');
+const {app, BrowserWindow, ipcMain} = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
+let resume = {
+    "title": "Chuojia Mo",
+    "description": "Hello, I am Jiajia",
+    "cv": [{
+        "id": 1,
+        "title": "Education",
+        "experiences": [
+            {
+                "id": 0,
+                "period": "sept. 2017 - present",
+                "description": "Education 1."
+            },
+            {
 
-function createWindow () {
-    win = new BrowserWindow({ width: 800, height: 600 });
+                "id": 0,
+                "period": "aug. 1800 - sept. 2017",
+                "description": "Education 2."
+            }
+        ]
+    }, {
+        "id": 2,
+        "title": "Experience",
+        "experiences": [
+            {
+                "id": 1,
+                "period": "sept. 2017 - present",
 
-    win.loadFile('index.html');
+                "description": "Experience 1." },
+            {
+                "id": 2,
+                "period": "aug. 800 - sept. 2017",
 
-    // win.webContents.openDevTools();
+                "description": "Experience 2."}
+        ]
+    }]
+};
+
+function main() {
+    win = new BrowserWindow({width: 800, height: 600});
+
+    ipcMain.on("show", (event) => {
+        event.sender.send('update', resume);
+    });
+
+    ipcMain.on('experience-remove', (event, args) => {
+        let section = args.section,
+            experience = args.experience;
+
+        resume.cv[section].experiences.splice(experience, 1);
+        console.log(resume.cv[section].experiences);
+        win.webContents.send('update', resume);
+    });
+
+    ipcMain.on('experience-edit', (event, args) => {
+        // resume[args.section].experiences[args.experience] = args.content;
+        // event.sender.send('update', resume);
+    });
+
+    ipcMain.on('experience-add', (event, args) => {
+        // resume[args.section][args.experience] = args.content;
+        // event.sender.send('update', resume);
+    });
+
 
     win.on('closed', () => {
         win = null
-    })
+    });
+    win.loadFile('index.html');
+
+
 }
 
-app.on('ready', createWindow);
+app.on('ready', main);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
@@ -27,7 +86,9 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
     if (win === null) {
-        createWindow()
+        main()
     }
 });
+
+
 
